80f0a1e16a1be77cb69a6a26975e25d9fa065765
Deepak <deepaktyagi57731@gmail.com>
2026-05-01 17:10:50 +0530

    update code

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,7 @@ const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 // Routes
@@ -21,3 +21,8 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT} (Bound to 0.0.0.0)`);
 });
+
+app.use(cors({
+  origin: "https://team-task-manager-15luht36t-deepaktyagi57731-6701s-projects.vercel.app", 
+  credentials: true
+}));
